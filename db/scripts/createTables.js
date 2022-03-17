186bad91ba256044df73944ae3c764b6fc80e529
Quinn.C <joanchenuk@gmail.com>
2022-03-17 11:37:44 +0000

    database table changed based on front end changed

--- db/scripts/createTables.js
+++ db/scripts/createTables.js
@@ -4,55 +4,49 @@
 import {query} from '../connections.js';
 
 const createTablesSqlString =
-    `CREATE TABLE IF NOT EXISTS teams (
-        team_id SERIAL PRIMARY KEY,
-        team_name varchar(255),
-        created_at timestamp not null default CURRENT_TIMESTAMP
-      );
-      
-      CREATE TABLE IF NOT EXISTS users (
+    `CREATE TABLE IF NOT EXISTS users (
         user_id int PRIMARY KEY,
         full_name varchar(255),
-        created_at timestamp not null default CURRENT_TIMESTAMP,
-        team_id int
+        email varchar(255),
+        phone varchar(255),
+        created_at timestamp not null default CURRENT_TIMESTAMP
       );
       
       CREATE TABLE IF NOT EXISTS projects (
         project_id SERIAL PRIMARY KEY,
         project_name varchar(255),
+        description varchar(255),
         created_at timestamp not null default CURRENT_TIMESTAMP
       );
       
       CREATE TABLE IF NOT EXISTS tickets (
         ticket_id SERIAL PRIMARY KEY,
         ticket_name varchar(255),
-        project_id int,
-        author varchar(255),
         description varchar(255),
+        project_id int,
+        ticket_author varchar(255),
+        user_id int,
         status varchar(255),
-        priority varchar(255),
         type varchar(255),
-        time_of_estimate int,
-        assigned_dev int,
+        priority varchar(255),
+        estimate_hours int,
         due_date date,
         created_at timestamp not null default CURRENT_TIMESTAMP
       );
       
-      CREATE TABLE IF NOT EXISTS user_project (
+      CREATE TABLE IF NOT EXISTS relationships (
         id SERIAL PRIMARY KEY,
-        user_id int,
-        project_id int
+        project_id int,
+        user_id int
       );
-      
-      ALTER TABLE users ADD FOREIGN KEY (team_id) REFERENCES teams (team_id);
       
       ALTER TABLE tickets ADD FOREIGN KEY (project_id) REFERENCES projects (project_id);
       
-      ALTER TABLE tickets ADD FOREIGN KEY (assigned_dev) REFERENCES users (user_id);
+      ALTER TABLE tickets ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
     
-      ALTER TABLE user_project ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
+      ALTER TABLE relationships ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
       
-      ALTER TABLE user_project ADD FOREIGN KEY (project_id) REFERENCES projects (project_id);`;
+      ALTER TABLE relationships ADD FOREIGN KEY (project_id) REFERENCES projects (project_id);`;
       
 
 async function createTables(){
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,56 @@
 import express from "express";
+import { getAllUsers, getUserById, createUser, deleteUserById, updateUser } from "../model/users/users";
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.json({ message: "all users data will be available soon" });
+/* GET all users listing. */
+router.get("/", function (req, res) {
+  const body = await getAllUsers();
+  res.json({ 
+    sucess: true,
+    message: body });
+});
+
+
+/* GET user by id. */
+router.get("/:user_id", function (req, res) {
+  //get user_id by params from the req
+  const user_id = Number(req.params.user_id)
+  const result = await getUserById(user_id);
+  res.json({ 
+    sucess: true,
+    message: result });
+});
+
+
+/* POST a new user. */
+router.post("/", function (req, res) {
+  //get body info from req.body
+  const body = req.body;
+  const result = await createUser();
+  res.json({ 
+    sucess: true,
+    message: result});
+});
+
+
+/* DELETE user by id. */
+router.delete("/:user_id", function (req, res) {
+  //get user_id by params from the req
+  const user_id = Number(req.params.user_id)
+  const result = await deleteUserById(user_id);
+  res.json({ 
+    sucess: true,
+    message: result});
+});
+
+
+/* GET all users listing. */
+router.put("/", function (req, res) {
+  const body = req.body;
+  const result = await updateUser(body);
+  res.json({ 
+    sucess: true,
+    message: result });
 });
 
 export default router;
