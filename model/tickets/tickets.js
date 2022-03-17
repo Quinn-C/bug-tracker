beef5a1684315c8323e221ec9533734f73ab93d5
Quinn.C <joanchenuk@gmail.com>
2022-03-17 12:12:17 +0000

    tickets model updated

--- model/tickets/tickets.js
+++ model/tickets/tickets.js
@@ -11,24 +11,24 @@ export async function getAllTickets(){
 
 //GET TICKET BY ID
 export async function getTicketById(id){
-    const res = await query(`SELET * FROM tickets WHERE team_id = $1;`, [id]);
+    const res = await query(`SELET * FROM tickets WHERE ticket_id = $1;`, [id]);
     return res.rows;
 }
 
 //CREATE TICKET
 export async function createTicket(body){
     ticket_name = body.ticket_name;
-    project_id = body.project_id;
-    author = body.author;
     description = body.description;
+    project_id = body.project_id;
+    ticket_author = body.ticket_author;
+    user_id = body.user_id;
     ticket_status = body.status;
-    priority = body.priority;
     type = body.type;
-    time_of_estimate = body.time_of_estimate;
-    assigned_dev = body.assigned_dev;
+    priority = body.priority;
+    estimate_hours = body.estimate_hours;
     due_date = body.due_date;
 
-    const res = await query(`INSERT INTO tickets(ticket_name, project_id, author, description, ticket_status, priority, type, time_of_estimate, assigned_dev, due_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;`, [ticket_name, project_id, author, description, ticket_status, priority, type, time_of_estimate, assigned_dev, due_date]);
+    const res = await query(`INSERT INTO tickets(ticket_name, description, project_id, ticket_author, user_id, status, type, priority, estimate_hour, due_date) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;`, [ticket_name, description, project_id, ticket_author, user_id, ticket_status, type, priority, estimate_hour, due_date]);
     return res.rows;
 }
 
@@ -43,15 +43,15 @@ export async function deleteTicketById(id){
 export async function updateTicket(body){
     ticket_id = body.ticket_id;
     ticket_name = body.ticket_name;
-    project_id = body.project_id;
-    author = body.author;
     description = body.description;
+    project_id = body.project_id;
+    ticket_author = body.ticket_author;
+    user_id = body.user_id;
     ticket_status = body.status;
-    priority = body.priority;
     type = body.type;
-    time_of_estimate = body.time_of_estimate;
-    assigned_dev = body.assigned_dev;
+    priority = body.priority;
+    estimate_hours = body.estimate_hours;
     due_date = body.due_date;
-    const res = await query(`UPDATE tickets SET ticket_name = $1, project_id = $2, author = $3, description = $4, ticket_status = $5, priority = $6, type = $7, time_of_estimate = $8, assigned_dev = $9, due_date = $10 WHERE ticket_id = $11,  RETURNING *`, [ticket_name, project_id, author, description, ticket_status, priority, type, time_of_estimate, assigned_dev, due_date, ticket_id]);
+    const res = await query(`UPDATE tickets SET ticket_name = $1, description = $2, project_id = $3, ticket_author = $4, user_id = $5, status = $6, type = $7, priority = $8, estimate_hour = $9, due_date = $10 WHERE ticket_id = $11, RETURNING *`, [ticket_name, description, project_id, ticket_author, user_id, ticket_status, type, priority, estimate_hour, due_date, ticket_id]);
     return res.rows;
 }
